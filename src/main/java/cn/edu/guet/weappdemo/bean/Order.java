68cf0a8d8582c9dfa004a3cc2d0b351946b34da5
iseven <hezs1026@qq.com>
2022-08-13 13:44:13 +0800

    订单列表功能的完善

--- src/main/java/cn/edu/guet/weappdemo/bean/Order.java
+++ src/main/java/cn/edu/guet/weappdemo/bean/Order.java
@@ -1,5 +1,7 @@
 package cn.edu.guet.weappdemo.bean;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +20,7 @@ public class Order {
     private String consume_type;
     private String table;
     private int payment_status;
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private java.sql.Timestamp create_time;
 
     public String getId() {
